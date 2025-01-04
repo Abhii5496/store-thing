5f25a8e0ab34085ea5c301fa852fd49088c8c5d1
Abhii <abhishekhsharma2002@gmail.com>
2025-01-04 19:51:41 +0530

    minor fix

--- src/components/ui/loading.jsx
+++ src/components/ui/loading.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div class="grid grid-cols-3 gap-1 w-[70px] h-[70px]">
-      <span class="w-full h-full bg-[#a5a5b0] animate-blink delay-0"></span>
-      <span class="w-full h-full bg-[#a5a5b0] animate-blink delay-200"></span>
-      <span class="w-full h-full bg-[#a5a5b0] animate-blink delay-300"></span>
-      <span class="w-full h-full bg-[#a5a5b0] animate-blink delay-400"></span>
-      <span class="w-full h-full bg-[#a5a5b0] animate-blink delay-500"></span>
-      <span class="w-full h-full bg-[#a5a5b0] animate-blink delay-600"></span>
+    <div className="grid grid-cols-3 gap-1 w-[70px] h-[70px]">
+      <span className="w-full h-full bg-[#a5a5b0] animate-blink delay-0"></span>
+      <span className="w-full h-full bg-[#a5a5b0] animate-blink delay-200"></span>
+      <span className="w-full h-full bg-[#a5a5b0] animate-blink delay-300"></span>
+      <span className="w-full h-full bg-[#a5a5b0] animate-blink delay-400"></span>
+      <span className="w-full h-full bg-[#a5a5b0] animate-blink delay-500"></span>
+      <span className="w-full h-full bg-[#a5a5b0] animate-blink delay-600"></span>
     </div>
   );
 }
