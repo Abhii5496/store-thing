06f5baf1264937c5df49642bfeda4cfbdee2d206
Abhii <abhishekhsharma2002@gmail.com>
2025-01-11 12:02:30 +0530

    exp 2

--- src/hooks/cart-hook.js
+++ src/hooks/cart-hook.js
@@ -1,173 +1,173 @@
-"use client";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { toast } from "sonner";
-import { useSession } from "next-auth/react";
-import { calculateTotalPrice } from "@/lib/utils";
-import useLocalData from "@/lib/local-store";
-import { connectDB } from "@/lib/mongodb";
+// "use client";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import { toast } from "sonner";
+// import { useSession } from "next-auth/react";
+// import { calculateTotalPrice } from "@/lib/utils";
+// import useLocalData from "@/lib/local-store";
+// import { connectDB } from "@/lib/mongodb";
 
-export const useCart = () => {
-  const [cartList, setCartList] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [totalAmount, setTotalAmount] = useState(0);
-  const { status, data: user } = useSession();
-  const {
-    addLocalCart,
-    cart,
-    clearLocalCart,
-    removeLocalCart,
-    updateLocalCart,
-  } = useLocalData();
+// export const useCart = () => {
+//   const [cartList, setCartList] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [totalAmount, setTotalAmount] = useState(0);
+//   const { status, data: user } = useSession();
+//   const {
+//     addLocalCart,
+//     cart,
+//     clearLocalCart,
+//     removeLocalCart,
+//     updateLocalCart,
+//   } = useLocalData();
 
-  // console.log("Cart-hook:", cartList);
+//   // console.log("Cart-hook:", cartList);
 
-  useEffect(() => {
-    if (status === "authenticated") {
-      fetchCartList();
-    }
-  }, [status]);
+//   useEffect(() => {
+//     if (status === "authenticated") {
+//       fetchCartList();
+//     }
+//   }, [status]);
 
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      setCartList(cart);
-      const total = calculateTotalPrice(cart);
-      setTotalAmount(total);
-    }
-  }, [status, cart]);
+//   useEffect(() => {
+//     if (status === "unauthenticated") {
+//       setCartList(cart);
+//       const total = calculateTotalPrice(cart);
+//       setTotalAmount(total);
+//     }
+//   }, [status, cart]);
 
-  const fetchCartList = async () => {
-    if (status === "unauthenticated") {
-      return;
-    }
+//   const fetchCartList = async () => {
+//     if (status === "unauthenticated") {
+//       return;
+//     }
 
-    setLoading(true);
-    await connectDB();
-    try {
-      const { data } = await axios.get("/api/cart/get");
-      // console.log(data);
-      setCartList(data.data);
-      setTotalAmount(data.totalPrice);
-    } catch (error) {
-      console.error("Error fetching cartList:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+//     setLoading(true);
+//     await connectDB();
+//     try {
+//       const { data } = await axios.get("/api/cart/get");
+//       // console.log(data);
+//       setCartList(data.data);
+//       setTotalAmount(data.totalPrice);
+//     } catch (error) {
+//       console.error("Error fetching cartList:", error);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
 
-  const addToCart = async (product) => {
-    if (status === "unauthenticated") {
-      return;
-    }
-    const { id, quantity, image, title, price } = product;
-    if (!loading) {
-      setLoading(true);
-      await connectDB();
+//   const addToCart = async (product) => {
+//     if (status === "unauthenticated") {
+//       return;
+//     }
+//     const { id, quantity, image, title, price } = product;
+//     if (!loading) {
+//       setLoading(true);
+//       await connectDB();
 
-      try {
-        if (status === "authenticated") {
-          const { data } = await axios.post("/api/cart/add", {
-            id,
-            quantity,
-            image,
-            title,
-            price,
-          });
-          // console.log("cartdata:", data);
-          setCartList(data.data);
-          setTotalAmount(data.totalPrice);
-        } else {
-          addLocalCart({ productId: id, quantity, image, title, price });
-        }
-        toast.success("Added to cart.");
-      } catch (error) {
-        console.error("Error adding to cartList:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
-  const updateCart = async (productId, quantity) => {
-    if (!loading) {
-      setLoading(true);
-      await connectDB();
+//       try {
+//         if (status === "authenticated") {
+//           const { data } = await axios.post("/api/cart/add", {
+//             id,
+//             quantity,
+//             image,
+//             title,
+//             price,
+//           });
+//           // console.log("cartdata:", data);
+//           setCartList(data.data);
+//           setTotalAmount(data.totalPrice);
+//         } else {
+//           addLocalCart({ productId: id, quantity, image, title, price });
+//         }
+//         toast.success("Added to cart.");
+//       } catch (error) {
+//         console.error("Error adding to cartList:", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+//   };
+//   const updateCart = async (productId, quantity) => {
+//     if (!loading) {
+//       setLoading(true);
+//       await connectDB();
 
-      try {
-        if (status === "authenticated") {
-          const { data } = await axios.post("/api/cart/update", {
-            productId,
-            quantity,
-          });
-          // console.log("cartdata:", data);
-          setCartList(data.data);
-          setTotalAmount(data.totalPrice);
-        } else {
-        }
-        await updateLocalCart(productId, quantity);
-        toast.success("Cart updated.");
-      } catch (error) {
-        console.error("Error adding to cart:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
+//       try {
+//         if (status === "authenticated") {
+//           const { data } = await axios.post("/api/cart/update", {
+//             productId,
+//             quantity,
+//           });
+//           // console.log("cartdata:", data);
+//           setCartList(data.data);
+//           setTotalAmount(data.totalPrice);
+//         } else {
+//         }
+//         await updateLocalCart(productId, quantity);
+//         toast.success("Cart updated.");
+//       } catch (error) {
+//         console.error("Error adding to cart:", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+//   };
 
-  const removeFromCart = async (productId) => {
-    if (!loading) {
-      setLoading(true);
-      await connectDB();
+//   const removeFromCart = async (productId) => {
+//     if (!loading) {
+//       setLoading(true);
+//       await connectDB();
 
-      try {
-        if (status === "authenticated") {
-          const { data } = await axios.post("/api/cart/remove", {
-            productId,
-          });
-          setCartList(data.data);
-          setTotalAmount(data.totalPrice);
-          // console.log("hookRem", data);
-        } else {
-          removeLocalCart(productId);
-        }
-        toast.success("Removed from cart.");
-      } catch (error) {
-        console.error("Error removing from cart.:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
-  const emptyCart = async () => {
-    if (!loading) {
-      setLoading(true);
-      await connectDB();
+//       try {
+//         if (status === "authenticated") {
+//           const { data } = await axios.post("/api/cart/remove", {
+//             productId,
+//           });
+//           setCartList(data.data);
+//           setTotalAmount(data.totalPrice);
+//           // console.log("hookRem", data);
+//         } else {
+//           removeLocalCart(productId);
+//         }
+//         toast.success("Removed from cart.");
+//       } catch (error) {
+//         console.error("Error removing from cart.:", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+//   };
+//   const emptyCart = async () => {
+//     if (!loading) {
+//       setLoading(true);
+//       await connectDB();
 
-      try {
-        if (status === "authenticated") {
-          const { data } = await axios.get("/api/cart/empty");
-          setCartList(data.data);
-          setTotalAmount(data.totalPrice);
-          // console.log("hookRem", data);
-        } else {
-          clearLocalCart();
-        }
-        toast.success("Cart empty.");
-      } catch (error) {
-        console.error("Error removing from cart.:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
+//       try {
+//         if (status === "authenticated") {
+//           const { data } = await axios.get("/api/cart/empty");
+//           setCartList(data.data);
+//           setTotalAmount(data.totalPrice);
+//           // console.log("hookRem", data);
+//         } else {
+//           clearLocalCart();
+//         }
+//         toast.success("Cart empty.");
+//       } catch (error) {
+//         console.error("Error removing from cart.:", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+//   };
 
-  return {
-    cartList,
-    loading,
-    addToCart,
-    updateCart,
-    removeFromCart,
-    fetchCartList,
-    totalAmount,
-    emptyCart,
-  };
-};
+//   return {
+//     cartList,
+//     loading,
+//     addToCart,
+//     updateCart,
+//     removeFromCart,
+//     fetchCartList,
+//     totalAmount,
+//     emptyCart,
+//   };
+// };
--- src/hooks/wishlist-hook.js
+++ src/hooks/wishlist-hook.js
@@ -1,109 +1,109 @@
-"use client";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { toast } from "sonner";
-import { useSession } from "next-auth/react";
-import useLocalData from "@/lib/local-store";
-import { connectDB } from "@/lib/mongodb";
+// "use client";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import { toast } from "sonner";
+// import { useSession } from "next-auth/react";
+// import useLocalData from "@/lib/local-store";
+// import { connectDB } from "@/lib/mongodb";
 
-export const useWishlist = () => {
-  const [wishlist, setWishlist] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const { status, data: user } = useSession();
+// export const useWishlist = () => {
+//   const [wishlist, setWishlist] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const { status, data: user } = useSession();
 
-  const { wishlist: wish, addLocalWish, removeLocalWish } = useLocalData();
+//   const { wishlist: wish, addLocalWish, removeLocalWish } = useLocalData();
 
-  useEffect(() => {
-    if (status === "authenticated") {
-      fetchWishlist();
-    }
-  }, [status]);
+//   useEffect(() => {
+//     if (status === "authenticated") {
+//       fetchWishlist();
+//     }
+//   }, [status]);
 
-  // useEffect(() => {
-  //   if (status === "unauthenticated") {
-  //     setWishlist(wish);
-  //   }
-  // }, [status, wish]);
+//   // useEffect(() => {
+//   //   if (status === "unauthenticated") {
+//   //     setWishlist(wish);
+//   //   }
+//   // }, [status, wish]);
 
-  // console.log("wishlist-hook", wishlist);
+//   // console.log("wishlist-hook", wishlist);
 
-  const fetchWishlist = async () => {
-    if (status === "unauthenticated") {
-      return;
-    }
-    await connectDB();
-    setLoading(true);
-    try {
-      const { data } = await axios.get("/api/wishlist/get");
-      setWishlist(data.data);
-    } catch (error) {
-      console.error("Error fetching wishlist:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+//   const fetchWishlist = async () => {
+//     if (status === "unauthenticated") {
+//       return;
+//     }
+//     await connectDB();
+//     setLoading(true);
+//     try {
+//       const { data } = await axios.get("/api/wishlist/get");
+//       setWishlist(data.data);
+//     } catch (error) {
+//       console.error("Error fetching wishlist:", error);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
 
-  const addToWishlist = async (product) => {
-    if (status === "unauthenticated") {
-      return;
-    }
+//   const addToWishlist = async (product) => {
+//     if (status === "unauthenticated") {
+//       return;
+//     }
 
-    if (!loading) {
-      await connectDB();
+//     if (!loading) {
+//       await connectDB();
 
-      setLoading(true);
-      try {
-        if (status === "authenticated") {
-          const { data } = await axios.post("/api/wishlist/add", product);
-          setWishlist(data.data);
-        }
-        // else {
-        //   addLocalWish({ ...product, productId: product.id });
-        // }
-        toast.success("Added to wishlist");
-      } catch (error) {
-        console.error("Error adding to wishlist:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
+//       setLoading(true);
+//       try {
+//         if (status === "authenticated") {
+//           const { data } = await axios.post("/api/wishlist/add", product);
+//           setWishlist(data.data);
+//         }
+//         // else {
+//         //   addLocalWish({ ...product, productId: product.id });
+//         // }
+//         toast.success("Added to wishlist");
+//       } catch (error) {
+//         console.error("Error adding to wishlist:", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+//   };
 
-  const removeFromWishlist = async (productId) => {
-    if (status === "unauthenticated") {
-      return;
-    }
-    if (!loading) {
-      await connectDB();
+//   const removeFromWishlist = async (productId) => {
+//     if (status === "unauthenticated") {
+//       return;
+//     }
+//     if (!loading) {
+//       await connectDB();
 
-      setLoading(true);
+//       setLoading(true);
 
-      try {
-        if (status === "authenticated") {
-          const { data } = await axios.post("/api/wishlist/remove", {
-            productId,
-          });
-          setWishlist(data.data);
-          // console.log("hookRem", data);
-        }
+//       try {
+//         if (status === "authenticated") {
+//           const { data } = await axios.post("/api/wishlist/remove", {
+//             productId,
+//           });
+//           setWishlist(data.data);
+//           // console.log("hookRem", data);
+//         }
 
-        // else {
-        //   removeLocalWish(productId);
-        // }
-        toast.success("Removed from wishlist");
-      } catch (error) {
-        console.error("Error removing from wishlist:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
+//         // else {
+//         //   removeLocalWish(productId);
+//         // }
+//         toast.success("Removed from wishlist");
+//       } catch (error) {
+//         console.error("Error removing from wishlist:", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+//   };
 
-  return {
-    wishlist,
-    loading,
-    addToWishlist,
-    removeFromWishlist,
-    fetchWishlist,
-  };
-};
+//   return {
+//     wishlist,
+//     loading,
+//     addToWishlist,
+//     removeFromWishlist,
+//     fetchWishlist,
+//   };
+// };
